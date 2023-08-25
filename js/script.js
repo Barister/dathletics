3a49fca3e301dd2f5cf3e9540cee6b788f459fac
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-08-25 15:27:01 +0600

    Header with burger ready

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//burger activation
+
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+
+   burger.classList.toggle('active');
+   headerMenu.classList.toggle('active');
+});
