7cefd2018d0deaadef364f3df40243a38648b4e4
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-08-29 15:37:14 +0600

    Catalog basic features sketchly done

--- js/script.js
+++ js/script.js
@@ -8,3 +8,61 @@ burger.addEventListener('click', () => {
    burger.classList.toggle('active');
    headerMenu.classList.toggle('active');
 });
+
+
+// catalog tabs
+
+
+
+// function changeTab(category) {
+//    const contents = document.querySelector('.tabs-catalog');
+//    console.log('contents:', contents);
+
+//    contents.addEventListener('click', (event) => {
+//       console.log('event:', event);
+
+//       let clickedTab = event.target;
+
+//       console.log('clickedTab:', clickedTab);
+
+
+//       return clickedTab.classList.toggle('active');
+//    });
+
+
+
+
+
+//    // contents.forEach(content => {
+
+//    //    console.log('content:', content);
+//    //    // content.
+
+//    //    const contentCategories = content.classList.contains(category) || category === 'all';
+//    //    if (contentCategories) {
+//    //       content.style.display = 'block';
+//    //    } else {
+//    //       content.style.display = 'none';
+//    //    }
+//    // });
+// }
+
+// Изначально показываем все карточки
+//changeTab('all');
+
+
+function changeTab(category) {
+   const contents = document.querySelectorAll('.cards-catalog__item');
+
+   console.log('contents:', contents);
+
+   contents.forEach(content => {
+      if (category === 'all' || content.dataset.category.includes(category)) {
+         content.style.display = 'flex';
+      } else {
+         content.style.display = 'none';
+      }
+   });
+}
+
+changeTab('all');
